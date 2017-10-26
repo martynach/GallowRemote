0a8d5d8e613a8e3aa53c234432161c6508ba316c
Martynka <Martynka@Martyna_Laptop>
2017-10-26 03:32:14 +0200

    gallows game improvements

--- Gallows/WebContent/games/gallows/GallowsGameScript.js
+++ Gallows/WebContent/games/gallows/GallowsGameScript.js
@@ -1,11 +1,17 @@
 const ALPHABET = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ'.split('');
+const BOARD_BEFORE_START_CLASS = 'board-before-start';
 const KEYBOARD_LETTER_CLASS = 'keyboard__letter';
+const KEYBOARD_RED_LETTER_CLASS = 'keyboard__red-letter';
+const KEYBOARD_GREEN_LETTER_CLASS = 'keyboard__green-letter';
+
 const NOT_FOUND_INDEX = -1;
+const VISIBLE_WORD_CLASS = '.js-visible-word';
 
 class GallowsGame {
   constructor() {
     this.keyboard = document.querySelector('.js-keyboard');
-    this.visibleWordContainer = document.querySelector('.js-visible-word');
+    this.keyboard.innerHTML = "";
+    this.visibleWordContainer = document.querySelector(VISIBLE_WORD_CLASS);
     this.picture = document.querySelector('.js-picture');
 
     this.word = 'DUPA JAS KARUZELA';
@@ -33,7 +39,7 @@ class GallowsGame {
       button.textContent = alphabetElement;
       button.className = KEYBOARD_LETTER_CLASS;// className to odpowiednik class w htmlu (bo class to slowko zarezerwowane)
 		// button.classList.add()
-      button.addEventListener('click', () => this.checkLetter(alphabetElement));
+    //  button.addEventListener('click', () => this.checkLetter(alphabetElement));
       
       //inna opcja:
       //button.addEventListener("click", checkLetter);
@@ -41,6 +47,11 @@ class GallowsGame {
       //function checkLetter(event) {
       //i w ten sposób trzeba by wydobyc literke:
       //const letter = event.target.textContent;
+      //to powyższe byloby prawda gdyby checkLetter było funkcja a nie metoda
+      //w przypadku metody: this.checkLetter.bind(this)
+      //jednak z tej opcji korzystam bo musze miec dostep do calego diva aby pozmieniac kolorki? dodac klase?
+      
+      button.addEventListener('click', this.checkLetter.bind(this));
     		
 
       this.keyboard.appendChild(button);
@@ -51,7 +62,10 @@ class GallowsGame {
     this.visibleWordContainer.textContent = this.visibleWord;
   }
 
-  checkLetter(letter) {
+  
+  checkLetter(event) {
+	  
+	let letter = event.target.textContent;
     const word = this.word;
     if (word.includes(letter)) {
     	//var sie bardzo rzadko uzywa: const albo let
@@ -68,15 +82,47 @@ class GallowsGame {
       }
 
       this.updateVisibleWord();
+      let element = event.target;
+      element.classList.add(KEYBOARD_GREEN_LETTER_CLASS);
+      if(this.visibleWord == this.word) {
+    	  this.gameWon();
+      }
     } else {
       this.misses++;
       //string interpolation
       this.picture.src = `img/gallow${this.misses}.png`;
+      
+      let element = event.target;
+      element.classList.add(KEYBOARD_RED_LETTER_CLASS);
+      
+      if(this.misses == 8) {
+    	  this.gameLost();
+      }
     }
+  }
+
+
+  gameLost() {
+	  this.keyboard.innerHTML = "<h3>You lost! </h3> <h4>Try harder next time!</h4>";
+	  let element = document.querySelector(VISIBLE_WORD_CLASS);
+	  element.classList.add(BOARD_BEFORE_START_CLASS);
+	  element.textContent = "START";
+
+  }
+  
+  gameWon() {
+	  this.keyboard.innerHTML = "<h3>Congratulations!</h3> <h4>You won!</h4>";
+	  let element = document.querySelector(VISIBLE_WORD_CLASS);
+	  element.classList.add(BOARD_BEFORE_START_CLASS);
+	  element.textContent = "START";
   }
 }
 
-new GallowsGame();
+function startGame() {
+	let element = document.querySelector(VISIBLE_WORD_CLASS);
+	element.classList.remove(BOARD_BEFORE_START_CLASS);
+	new GallowsGame();
+}
 
 
 
