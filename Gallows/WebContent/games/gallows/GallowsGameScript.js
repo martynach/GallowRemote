42247992ee7203fa7b1ab67fddafb97364d02ba0
Martynka <Martynka@Martyna_Laptop>
2017-10-30 02:16:47 +0100

    continuing with Gallows game

--- Gallows/WebContent/games/gallows/GallowsGameScript.js
+++ Gallows/WebContent/games/gallows/GallowsGameScript.js
@@ -1,3 +1,8 @@
+//import GallowsGameTimer from './GallowsGameTimer.js';
+
+const WORDS_TO_GUESS = 'BEZ PRACY NIE MA KOŁACZY, DUPA JAŚ KARUZELA, KTO TY JESTEŚ POLAK MAŁY, MURZYNEK BAMBO W AFRYCE MIESZKA, MIEJ SERCE PATRZ W SERCE'.split(',');	
+
+
 const ALPHABET = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ'.split('');
 const BOARD_BEFORE_START_CLASS = 'board-before-start';
 const KEYBOARD_LETTER_CLASS = 'keyboard__letter';
@@ -13,8 +18,10 @@ class GallowsGame {
     this.keyboard.innerHTML = "";
     this.visibleWordContainer = document.querySelector(VISIBLE_WORD_CLASS);
     this.picture = document.querySelector('.js-picture');
+    this.picture.src = 'img/gallow0.png';
 
-    this.word = 'DUPA JAS KARUZELA';
+    this.word = this.getWordToGuess();
+
     this.visibleWord = this.word.replace(/[^ ]/g, '_');
     this.misses = 0;
 
@@ -24,6 +31,11 @@ class GallowsGame {
 
     this.setupKeyboard();
     this.updateVisibleWord();
+  }
+  
+  getWordToGuess() {
+		let number = Math.floor(Math.random() * WORDS_TO_GUESS.length);
+		return WORDS_TO_GUESS[number];
   }
 
   setupKeyboard() {
@@ -67,62 +79,250 @@ class GallowsGame {
 	  
 	let letter = event.target.textContent;
     const word = this.word;
-    if (word.includes(letter)) {
-    	//var sie bardzo rzadko uzywa: const albo let
-    	//let i var rozny scope: doczytac
-      for (
-        let i = word.indexOf(letter);
-        i != NOT_FOUND_INDEX;
-        i = word.indexOf(letter, i + 1)
-      ) {
-        this.visibleWord =
-          this.visibleWord.substring(0, i) +
-          letter +
-          this.visibleWord.substring(i + 1, word.length);
-      }
-
-      this.updateVisibleWord();
-      let element = event.target;
-      element.classList.add(KEYBOARD_GREEN_LETTER_CLASS);
-      if(this.visibleWord == this.word) {
-    	  this.gameWon();
-      }
-    } else {
-      this.misses++;
-      //string interpolation
-      this.picture.src = `img/gallow${this.misses}.png`;
-      
-      let element = event.target;
-      element.classList.add(KEYBOARD_RED_LETTER_CLASS);
-      
-      if(this.misses == 8) {
-    	  this.gameLost();
-      }
+    if(!this.visibleWord.includes(letter)) {
+	    if (word.includes(letter)) {
+	    	//var sie bardzo rzadko uzywa: const albo let
+	    	//let i var rozny scope: doczytac
+	      for (
+	        let i = word.indexOf(letter);
+	        i != NOT_FOUND_INDEX;
+	        i = word.indexOf(letter, i + 1)
+	      ) {
+	        this.visibleWord =
+	          this.visibleWord.substring(0, i) +
+	          letter +
+	          this.visibleWord.substring(i + 1, word.length);
+	      }
+	
+	      this.updateVisibleWord();
+	      let element = event.target;
+	      element.classList.add(KEYBOARD_GREEN_LETTER_CLASS);
+	      if(this.visibleWord == this.word) {
+	    	  this.gameWon();
+	    	  timer.endTimer();
+	      }
+	    } else {
+	      this.misses++;
+	      //string interpolation
+	      this.picture.src = `img/gallow${this.misses}.png`;
+	      
+	      let element = event.target;
+	      element.classList.add(KEYBOARD_RED_LETTER_CLASS);
+	      
+	      if(this.misses == 8) {
+	    	  this.gameLost("Pomyliłeś/aś się aż 8 razy i wykonano na Tobie egzekucję...");
+	    	  timer.endTimer();
+	      }
+	    }
     }
   }
 
 
-  gameLost() {
-	  this.keyboard.innerHTML = "<h3>You lost! </h3> <h4>Try harder next time!</h4>";
-	  let element = document.querySelector(VISIBLE_WORD_CLASS);
-	  element.classList.add(BOARD_BEFORE_START_CLASS);
-	  element.textContent = "START";
+  gameLost(reason) {
+	  this.picture.src = 'img/gallow8.png';
+	  let message = "<h4>Powieszony!!! </h4>";
+	  message += "<h5>" + reason + "</h5>";
+	  
+	  scoreManager.takeLive();
+	  if(scoreManager.getLives() > 0) {
+		  if(scoreManager.getLives() == 1) {
+			  message += "<p>Nie martw się, pozostało Ci jeszcze " + scoreManager.getLives() + " życie.";
+		  } else {
+			  message += "<p>Nie martw się, pozostały Ci jeszcze " + scoreManager.getLives() + " życia.";
+		  }
+		  message += "<br />";
+		  message += '<div onclick="startGame();" class="continue-button" ><h3>KONTYNUUJ</h3></div>';
+
+	  } else {
+		  message += "<br />";
+		  message += "<h2>KONIEC GRY</h2>";
+		  let element = document.querySelector(VISIBLE_WORD_CLASS);
+		  element.classList.add(BOARD_BEFORE_START_CLASS);
+		  element.textContent = "START";
+	  } 
+	  
+	  this.keyboard.innerHTML = message
+
 
   }
   
   gameWon() {
-	  this.keyboard.innerHTML = "<h3>Congratulations!</h3> <h4>You won!</h4>";
+	  let message = '<h4>Gratulacje!</h4>';
+	  message += '<p>Tym razem udało Ci się uniknąć śmierci...</p>';
+	  message += '<br />';
+	  message += '<div onclick="startGame();" class="continue-button" ><h3>KONTYNUUJ</h3></div>';
+	  
+	  this.keyboard.innerHTML = message;
 	  let element = document.querySelector(VISIBLE_WORD_CLASS);
-	  element.classList.add(BOARD_BEFORE_START_CLASS);
-	  element.textContent = "START";
+//	  element.classList.add(BOARD_BEFORE_START_CLASS);
+//	  element.textContent = "Kontynuuj";
+	  
+	  let score = 3 + Math.floor(timer.getSecondsLeft() / 6);
+	  scoreManager.addUsersScore(score);
+	  scoreManager.changeToNextLevel();
+	  scoreManager.displayScore();	  
   }
 }
+
 
 function startGame() {
 	let element = document.querySelector(VISIBLE_WORD_CLASS);
 	element.classList.remove(BOARD_BEFORE_START_CLASS);
-	new GallowsGame();
+	startNewRound();
+
 }
+
+function startNewRound() {
+	game = new GallowsGame();
+	timer = new GallowsGameTimer(2, 0);
+	scoreManager.displayScore();
+}
+
+//**************************************************************************************************
+//should be in another file but it needs to be served by server not from disc in order import to work
+class GallowsGameTimer {
+	
+	constructor(minutes, seconds) {
+		this.timerContainer = document.querySelector(".js-timer");
+		this.minutes = minutes;
+		this.seconds = seconds;
+		this.flag = true;
+		this.startTimer();
+
+	}
+	
+	startTimer() {
+		if(!this.flag) {
+			return;
+		}
+		
+		this.displayTime();
+		
+		if(this.seconds == 0) {
+			this.seconds = 59;
+			if(this.minutes == 0) {
+				this.endTimer();
+				game.gameLost("Zakończył się limit czasu i wykonano przedwczesną egzekucję...");
+			} else {
+				this.minutes--;
+			}		
+		} else {
+			this.seconds--;
+		}
+		
+			setTimeout(this.startTimer.bind(this), 1000);	
+	}
+	
+	
+	displayTime() {
+		let displayMinutes;
+		if(this.minutes < 10) {
+			displayMinutes = "0" + this.minutes;
+		} else {
+			displayMinutes = this.minutes;
+		}
+		
+		let displaySeconds;
+		if(this.seconds < 10) {
+			displaySeconds = "0" + this.seconds;
+		} else {
+			displaySeconds = this.seconds;
+		}
+		
+		let display = '<p style="font-size: 20px">Do końca gry pozostało:</p>';
+		//display += "<br />";
+		display += '<p>' + displayMinutes + ":" + displaySeconds + '</p>';
+		this.timerContainer.innerHTML = display;
+	}
+	
+	endTimer() {
+		this.flag = false;
+		this.timerContainer.innerHTML = "";
+	}
+	
+	getSecondsLeft() {
+		return this.minutes * 60 + this.seconds;
+	}
+}
+
+//*****************************************************
+class ScoreManager {
+	constructor(userScore, scoresPerLevel, level, lives) {
+		this.score = userScore;
+		this.scoresPerLevel = scoresPerLevel;
+		this.level = level;
+		this.lives = lives;
+		
+		this.leftScores = this.scoresPerLevel - (this.score % (this.scoresPerLevel * this.level));
+		this.scoreContainer = document.querySelector(".js-score");
+		this.nextLevel = false;
+	}
+	
+	getScoreSummary() {
+		let summary = "<p>Twoje punkty: <b>" + this.score + "</b></p>";
+		
+		summary += "<p>Osiągnięty poziom: " + this.level + "</b></p>";
+		
+		summary += "<p>Pozostałe życia: <b>" + this.lives + "</b><p>";
+		
+		summary += "<p>Pozostało do kolejnego poziomu: <b>" + this.leftScores + "<b></p>";
+		
+		return summary;
+	}
+	
+	displayScore() {
+
+		
+		this.scoreContainer.innerHTML = this.getScoreSummary();
+	}
+	
+	hideScore() {
+		scoreContainer.innerHTML = "";
+	}
+	
+	addUsersScore(score) {
+		this.score += score;
+		this.leftScores = this.scoresPerLevel - (this.score % (this.scoresPerLevel * this.level));
+		if(this.leftScores == 0) {
+			this.nextLevel = true;
+		} else if(this.leftScores > this.scoresPerLevel) {
+			this.nextLevel = true;
+			this.leftScores -= this.scoresPerLevel;
+		}
+	}
+	
+	/*
+	 * returns true when player has enough score to go to next level
+	 */
+	isNextLevelAvailable() {
+		return this.nextLevel;
+	}
+	
+	/*
+	 * returns true when changing to next level was possible
+	 */
+	changeToNextLevel() {
+		if(this.isNextLevelAvailable()) {
+			this.level++;
+			this.leftScores -= this.leftScores - this.scoresPerLevel;
+			return true;
+		}
+		return false;
+	}
+	
+	takeLive() {
+		this.lives--;
+	}
+	
+	getLives() {
+		return this.lives;
+	}
+}
+
+/*GLOBAL VARIABLES*/
+let game;
+let timer;
+let scoreManager = new ScoreManager(0, 10, 1, 3);
 
 
 
